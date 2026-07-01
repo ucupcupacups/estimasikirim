1c88896cb5e6965e6e0409d979ff29cb15ebed83
Rizky Agung Setiawan <154102458+ucupcupacups@users.noreply.github.com>
2026-07-01 17:48:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -557,13 +557,14 @@ styles = getSampleStyleSheet()
 
 story = []
 
-story.append(Paragraph("<b> Godeliva Gift Serpong</b>", styles["Title"]))
-story.append(Paragraph("<b>Estimasi Harga dan Pengiriman</b>", styles["Title"]))
-story.append(Paragraph("<b>==========================================</b>", styles["Title"]))
+story.append(Paragraph("<b>Godeliva Gift Serpong</b>", styles["Title"]))
+story.append(Paragraph("<b>Estimasi Harga dan Pengiriman</b>", styles["Heading2"]))
+story.append(Paragraph("<br/>", styles["Normal"]))
+
 story.append(Paragraph(f"<b>Kota Tujuan :</b> {kota_pilihan}", styles["Normal"]))
 story.append(Paragraph(f"<b>Tanggal Kirim :</b> {tanggal_kirim.strftime('%d-%m-%Y')}", styles["Normal"]))
 story.append(Paragraph(f"<b>Berat :</b> {berat} Kg", styles["Normal"]))
-story.append(Paragraph("=========================================================================", styles["Normal"]))
+story.append(Paragraph("<br/>", styles["Normal"]))
 
 for i in range(len(hasil)):
 
